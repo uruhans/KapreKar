808c9fb388ce6c19314ab4c69424bd8a8eef6c91
uruhans <uruhans@gmail.com>
2025-07-02 13:42:46 +0200

    Generate Notification at startup. This Notification kan deepLink into DeeplinkingGuideTypeSafeNavigation app

--- app/src/main/java/com/urh/kaprekar/MainApplication.kt
+++ app/src/main/java/com/urh/kaprekar/MainApplication.kt
@@ -1,10 +1,23 @@
 package com.urh.kaprekar
 
+import android.annotation.SuppressLint
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.content.getSystemService
+import androidx.core.net.toUri
 import com.urh.kaprekar.di.appModules
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.GlobalContext.startKoin
+
+const val DEEPLINK_DOMAIN = "pl-coding.com"
 
 class MainApplication: Application() {
     override fun onCreate() {
@@ -15,5 +28,48 @@ class MainApplication: Application() {
             androidContext(applicationContext)
             modules(appModules)
         }
+
+        createNotificationChannel()
+        showNotification()
+    }
+
+    private fun createNotificationChannel() {
+        val channel = NotificationChannel(
+            "channel_id",
+            "channel_name",
+            NotificationManager.IMPORTANCE_DEFAULT
+        )
+        val notificationManager = getSystemService<NotificationManager>()!!
+        notificationManager.createNotificationChannel(channel)
+    }
+
+    @SuppressLint("QueryPermissionsNeeded")
+    private fun showNotification() {
+        val intent = Intent().apply {
+            component = ComponentName("com.plcoding.deeplinkingguidetypesafenavigation", "com.plcoding.deeplinkingguidetypesafenavigation.MainActivity")
+            //DeepLink
+            data = "https://$DEEPLINK_DOMAIN/VC3-9999XY".toUri()
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        }
+
+        val pendingIntent = PendingIntent.getActivity(
+            this,
+            1,
+            intent,
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        )
+
+        val notification = NotificationCompat.Builder(this, "channel_id")
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("Open App Y")
+            .setContentText("Tap to launch deeplink in DeeplinkingGuideTypeSafeNavigation")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent) // This is key
+            .setAutoCancel(true)
+            .build()
+
+        val notificationManager = this.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(1, notification)
+
     }
 }
--- app/src/main/java/com/urh/kaprekar/MainScreen.kt
+++ app/src/main/java/com/urh/kaprekar/MainScreen.kt
@@ -2,6 +2,11 @@
 
 package com.urh.kaprekar
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.AnimatedVisibilityScope
 import androidx.compose.animation.ExperimentalSharedTransitionApi
@@ -9,23 +14,32 @@ import androidx.compose.animation.SharedTransitionScope
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.scaleIn
 import androidx.compose.animation.scaleOut
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.consumeWindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.rounded.ThumbUp
+import androidx.compose.material3.Button
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.urh.kaprekar.ui.theme.KapreKarTheme
 import org.koin.androidx.compose.koinViewModel
@@ -44,6 +58,22 @@ fun SharedTransitionScope.MainScreen(
         derivedStateOf {
             state.code.joinToString("").toInt()
         }
+    }
+
+    val context = LocalContext.current
+    var hasPermission by remember {
+        mutableStateOf(if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            ContextCompat.checkSelfPermission(
+                context,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) == PackageManager.PERMISSION_GRANTED
+        } else true)
+    }
+
+    val permissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        hasPermission = isGranted
     }
 
    Scaffold(
@@ -77,50 +107,63 @@ fun SharedTransitionScope.MainScreen(
 
        }
    ) { innerPadding ->
-
-       val focusRequesters = remember {
-           List(4) { FocusRequester() }
-       }
-       val focusManager = LocalFocusManager.current
-       val keyboardManager = LocalSoftwareKeyboardController.current
-
-       LaunchedEffect(state.focusedIndex) {
-           state.focusedIndex?.let { index ->
-               focusRequesters.getOrNull(index)?.requestFocus()
-           }
-       }
-
-       LaunchedEffect(state.code, keyboardManager) {
-           val allNumbersEntered = state.code.none { it == null }
-           if(allNumbersEntered) {
-               focusRequesters.forEach {
-                   it.freeFocus()
+       if(!hasPermission && Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+           Box(
+               modifier = Modifier
+                   .fillMaxSize(),
+               contentAlignment = Alignment.Center
+           ) {
+               Button(onClick = {
+                   permissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+               }) {
+                   Text(text = "Request permission")
                }
-               focusManager.clearFocus()
-               keyboardManager?.hide()
            }
-       }
+       } else {
+           val focusRequesters = remember {
+               List(4) { FocusRequester() }
+           }
+           val focusManager = LocalFocusManager.current
+           val keyboardManager = LocalSoftwareKeyboardController.current
 
-       NumberScreen(
-           state = state,
-           focusRequesters = focusRequesters,
-           onAction = { action ->
-               when(action) {
-                   is NumberAction.OnEnterNumber -> {
-                       if(action.number != null) {
-                           focusRequesters[action.index].freeFocus()
-                       }
+           LaunchedEffect(state.focusedIndex) {
+               state.focusedIndex?.let { index ->
+                   focusRequesters.getOrNull(index)?.requestFocus()
+               }
+           }
+
+           LaunchedEffect(state.code, keyboardManager) {
+               val allNumbersEntered = state.code.none { it == null }
+               if(allNumbersEntered) {
+                   focusRequesters.forEach {
+                       it.freeFocus()
                    }
-                   else -> Unit
+                   focusManager.clearFocus()
+                   keyboardManager?.hide()
                }
-               viewModel.onAction(action)
-           },
-           onDelete = {
-               viewModel.onDelete()
-           },
-           modifier = Modifier
-               .padding(innerPadding)
-               .consumeWindowInsets(innerPadding)
-       )
+           }
+
+           NumberScreen(
+               state = state,
+               focusRequesters = focusRequesters,
+               onAction = { action ->
+                   when(action) {
+                       is NumberAction.OnEnterNumber -> {
+                           if(action.number != null) {
+                               focusRequesters[action.index].freeFocus()
+                           }
+                       }
+                       else -> Unit
+                   }
+                   viewModel.onAction(action)
+               },
+               onDelete = {
+                   viewModel.onDelete()
+               },
+               modifier = Modifier
+                   .padding(innerPadding)
+                   .consumeWindowInsets(innerPadding)
+           )
+       }
    }
 }
